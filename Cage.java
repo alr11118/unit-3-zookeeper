893f8650417fb19b6f6e4cfd609b6b2115723cb1
Alara Engin <alara.engin2008@gmail.com>
2026-01-16 09:37:39 -0800

    cage class

--- Cage.java
+++ Cage.java
@@ -1,23 +1,35 @@
 public class Cage {
 
     // Create a private instance variable of type Animal. Don't instantiate the variable, only declare it
-    
+    private Animal animal;
     // No-arg constructor:
     // - Instantiates this cage with a random Animal.
     // Hint: Animal() can choose a random type.
-
-
+    public Cage()
+    {
+      this.animal = new Animal();
+    }
     // Constructor with Animal parameter:
     // - Places that Animal into the cage.
     // - Use this.animal to avoid shadowing confusion.
-
+    public Cage(Animal animal)
+    {
+      this.animal = new Animal(animal.getAnimalType());
+    }
     // Write a Getter/Accessor for returning the Animal instance variable
-
-
+    public Animal getAnimal()
+    {
+      return this.animal;
+    }
     // Write a Setter/Mutator for editing the Animal instance variable
-
-
+    public void setAnimal(Animal newAnimal)
+    {
+      this.animal = new Animal(newAnimal.getAnimalType());
+    }
     // toString():
     // - Friendly cage description including the animal.
-
+    public String toString()
+    {
+      return ("Animal in the cage: " + this.animal);
+    }
 }
